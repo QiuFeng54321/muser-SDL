f4c7c66b08a8b5be2152d0870ff23b1628de8d2a
Qiufeng54321 <williamcraft@163.com>
2020-07-21 14:46:21 +0800

    removed build dir and make use of sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-#include <SDL.h>
+/*This source code copyrighted by Lazy Foo' Productions (2004-2020)
+and may not be redistributed without written permission.*/
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+//Using SDL and standard IO
+#include "SDL.h"
+#include <stdio.h>
+
+int main( int argc, char* args[] )
+{
+    printf("Hi\n");
+	SDL_Init(SDL_INIT_VIDEO);
+    SDL_Delay(5 * 1000);
+    SDL_Quit();
+    return 0;
 }
