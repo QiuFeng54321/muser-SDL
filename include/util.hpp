d3c26cbb518d1415fcd6d8806ed5f21599a7551c
Qiufeng54321 <williamcraft@163.com>
2020-07-30 19:02:37 +0800

    Added constrain function.
    Signed-off-by: Qiufeng54321 <williamcraft@163.com>

--- include/util.hpp
+++ include/util.hpp
@@ -4,9 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <type_traits>
+#include <cassert>
 
 namespace muser::util {
     std::string random_template_file();
+    template <typename T, bool check = std::is_arithmetic<T>::value>
+    T constrain(T val, T lower_bound, T upper_bound) {
+        assert(check);
+        if (val > upper_bound) return upper_bound;
+        else if (val < lower_bound) return lower_bound;
+        return val;
+    }
 }  // namespace muser::util
 
 #endif
